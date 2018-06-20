7596d873e882092d1723eb2ae8f078c342c4ddf9
Wilder Pereira <wilder_roberto@hotmail.com>
2018-06-19 22:26:08 -0300

    Setup initial data with h2

--- src/main/java/com/wilderpereira/demospring/DemoSpringApplication.java
+++ src/main/java/com/wilderpereira/demospring/DemoSpringApplication.java
@@ -2,10 +2,11 @@ package com.wilderpereira.demospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class DemoSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringApplication.class, args);
 	}
---
+++ src/main/java/com/wilderpereira/demospring/configuration/RunnerConfiguration.java
@@ -0,0 +1,32 @@
+package com.wilderpereira.demospring.configuration;
+
+import com.wilderpereira.demospring.domain.Student;
+import com.wilderpereira.demospring.repository.StudentRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class RunnerConfiguration {
+
+    @Bean
+    ApplicationRunner init(StudentRepository studentRepository) {
+        return args -> {
+            List<Student> mockStudents = Arrays.asList(
+                    new Student("Wilder", 20),
+                    new Student("Everton", 20),
+                    new Student("Murilo", 20),
+                    new Student("Vinícius", 20),
+                    new Student("André", 20),
+                    new Student("Kleber", 20),
+                    new Student("JC", 20)
+            );
+            studentRepository.saveAll(mockStudents);
+            System.out.println(studentRepository.findAll());
+        };
+    }
+
+}
